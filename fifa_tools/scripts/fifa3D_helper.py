60d2b0ba6397a71df512c1d5ef258a533241409c
DeathGOD7 <36422471+DeathGOD7@users.noreply.github.com>
2021-11-26 19:41:56 +0545

    updated fifa_helper and added .vscode settings

--- fifa_tools/scripts/fifa3D_helper.py
+++ fifa_tools/scripts/fifa3D_helper.py
@@ -10,15 +10,14 @@ import zlib, struct
 from enum import Enum
 
 def List_To_Tuple(lst):
-    return tuple(List_To_Tuple(i) if isinstance(i, list) else i for i in lst)
+	return tuple(List_To_Tuple(i) if isinstance(i, list) else i for i in lst)
 
-def Add_Vgroup_To_Objects(vg_indices, vg_weights, vg_name, objects):
-    assert(len(vg_indices) == len(vg_weights))
-    if vg_indices:
-        for obj in objects:
-            # We replace/override here...
-            vg = obj.vertex_groups.get(vg_name)
-            if vg is None:
-                vg = obj.vertex_groups.new(name=vg_name)
-            for i, w in zip(vg_indices, vg_weights):
-                vg.add((i,), w, 'REPLACE') 
+def Add_Vgroup_To_Objects(vg_indices, vg_weights, vg_name, obj):
+	assert(len(vg_indices) == len(vg_weights))
+	if vg_indices:
+		# We replace/override here...
+		vg = obj.vertex_groups.get(vg_name)
+		if vg is None:
+			vg = obj.vertex_groups.new(name=vg_name)
+		for i, w in zip(vg_indices, vg_weights):
+			vg.add((i,), w, 'REPLACE') 
